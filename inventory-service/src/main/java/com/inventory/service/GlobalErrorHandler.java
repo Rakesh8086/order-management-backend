2fffe57c5cc8712165090631253c6f636ca83957
Rakesh8086 <rakeshteja7007@gmail.com>
2025-12-29 13:47:12 +0530

    global error handler added to catch and display field validation errors

---
+++ inventory-service/src/main/java/com/inventory/service/GlobalErrorHandler.java
@@ -0,0 +1,33 @@
+package com.inventory.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalErrorHandler {
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName;
+            if(error instanceof FieldError fieldError) {
+                fieldName = fieldError.getField();
+            } 
+            else {
+                fieldName = error.getObjectName(); 
+            }
+            String message = error.getDefaultMessage();
+            errorMap.put(fieldName, message);
+        });
+        
+        // 400 Bad request
+        return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST); 
+    }
+}
