950afe480bbc9c65a10f12d954e5fb1e371d704b
Rakesh8086 <rakeshteja7007@gmail.com>
2025-12-31 02:22:27 +0530

    open feign client setup for inter service communication

---
+++ order-service/src/main/java/com/order/service/feign/ProductClient.java
@@ -0,0 +1,19 @@
+package com.order.service.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.order.service.response.ProductResponseAdmin;
+
+@FeignClient(name = "product-service", url = "http://localhost:8081") 
+public interface ProductClient {
+    @GetMapping("/api/products/id/{id}")
+    ProductResponseAdmin getById(@PathVariable Long id);
+
+    @PatchMapping("/api/products/update/{id}/stock")
+    void updateStock(@PathVariable Long id, 
+    		@RequestParam Integer quantityChange);
+}
